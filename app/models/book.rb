58829e9c7ad33a882f911e857b9687bee3479968
EC2 Default User <ec2-user@ip-172-31-1-93.ap-northeast-1.compute.internal>
2025-02-24 13:51:05 +0000

    処理終了、見た目も調整

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ApplicationRecord
 
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
+
   has_one_attached :image
   belongs_to :user
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
 
